a0a10fbe1dfb50f759b234c3ebee4be443b1acdb
aby-fierro <abygailfierro191@gmail.com>
2026-07-30 02:32:45 -0600

    Fix celular y GPS directo a WhatsApp

--- app/main.py
+++ app/main.py
@@ -1,15 +1,12 @@
 ﻿from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/", response_class=RedirectResponse)
-async def read_root():
+async def inicio():
     return RedirectResponse(url="/registro")
 
 @app.get("/registro", response_class=HTMLResponse)
@@ -17,7 +14,6 @@ async def registro(request: Request):
     return templates.TemplateResponse(request=request, name="registro.html")
 
 @app.get("/p/dante", response_class=HTMLResponse)
+@app.get("/p/dante/", response_class=HTMLResponse)
 async def perfil_dante(request: Request):
     return templates.TemplateResponse(request=request, name="dante.html")
-
-
