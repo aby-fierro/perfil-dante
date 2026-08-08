48a3053e7e542b3a33d491cb1bae98cc48928cbd
aby-fierro <abygailfierro191@gmail.com>
2026-08-08 17:29:58 -0600

    Agregado perfil boris y ruta estatica

---
+++ app/main.py
@@ -0,0 +1,14 @@
+﻿from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/p/boris", response_class=HTMLResponse)
+@app.get("/p/boris/", response_class=HTMLResponse)
+async def ver_boris(request: Request):
+    return templates.TemplateResponse(request=request, name="gato.html")
